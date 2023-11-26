fa84ceb4e8aa4eeece153bbf4b6fd38975d8d08e
Mark Morabito <mmorabito@users.noreply.github.com>
2023-11-26 10:38:48 -1000

    start of p5 button

--- js/p5-1.js
+++ js/p5-1.js
@@ -1,5 +1,20 @@
+let input, button, greeting;
+
 function setup() {
     createCanvas(400, 400);
+    
+    input = createInput();
+    input.position(20,65);
+
+    button = createButton('submit');
+    button.position(input.x + input.width, 65);
+    button.mousePressed(greet);
+
+    greeting = createElement('h2', 'what is your name?');
+    greeting.position(20,5);
+
+    textAlign(CENTER);
+    textSize(50);
   }
   
   function draw() {
